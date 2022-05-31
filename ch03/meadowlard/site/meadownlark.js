30e2b9441ebe50b9c612fdf74f3cefce72ca244d
Bob Tang <254202042@qq.com>
2022-05-31 11:24:47 +0800

    feat:表单处理

--- ch03/meadowlard/site/meadownlark.js
+++ ch03/meadowlard/site/meadownlark.js
@@ -103,11 +103,17 @@ app.get('/newsletter', function(req,res) {
 })
 
 app.post('/process', function(req,res) {
-    console.log("Form (from querystring): " + req.query.form)
-    console.log("CSRF token(from hidden form field): " + req.body._csrf)
-    console.log('Name (from visible form field): ' + req.body.name); 
-    console.log('Email (from visible form field): ' + req.body.email); 
-    res.redirect(303, '/thank-you');
+    // console.log("Form (from querystring): " + req.query.form)
+    // console.log("CSRF token(from hidden form field): " + req.body._csrf)
+    // console.log('Name (from visible form field): ' + req.body.name); 
+    // console.log('Email (from visible form field): ' + req.body.email); 
+    // res.redirect(303, '/thank-you');
+
+    if (req.xhr || req.accepts('json,html') === 'json') {
+        res.send({success:true})
+    } else {
+        res.redirect(303,'thank-you')
+    }
 })
 
 app.use(function(req,res,next) {
